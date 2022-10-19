92601dda3f909ca80a155d90ec6813277eb13402
Ariady Putra <ariady.putra@gmail.com>
2022-10-19 21:18:58 +0700

    Preload chunk attempt

--- frontend/src/component/ConnectWallet.js
+++ frontend/src/component/ConnectWallet.js
@@ -4,6 +4,9 @@ function ConnectWallet({setWallet, setMusicLibrary, reportStatus, reportError}) 
   const [wallets, setWallets] = React.useState(() => []);
   
   React.useEffect(() => {
+    if(window.cardano) // pre-load chunk
+      import('@emurgo/cardano-serialization-lib-asmjs');
+    
     const wallets = [];
     for(const key in window.cardano) {
       if(window.cardano[key].enable && wallets.indexOf(key) < 0) {
