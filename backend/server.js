6d67a5f30e1bee1c4f71ebe8b6c07edd53546aab
Ariady Putra <ariady.putra@gmail.com>
2023-12-17 11:17:40 +0700

    Use Blockfrost ProjectID from env var

--- backend/server.js
+++ backend/server.js
@@ -1,67 +1,90 @@
-const { BlockFrostAPI } = require('@blockfrost/blockfrost-js');
+const { BlockFrostAPI } = require("@blockfrost/blockfrost-js");
 async function run(api, rsp) {
   try {
     rsp.json(await api);
-  }
-  catch(x) {
+  } catch (x) {
     rsp.json(x);
   }
 }
 
 var API;
 
-const { env } = require('process');
-const express = require('express');
-const app     = express();
-app.listen(env.HEROKU && env.PORT || 55555, () => {
-  const { exec } = require('child_process');
-  exec('cat ~/cardano/cfg/pid.bf', (x,o,e) => {
-    API = new BlockFrostAPI({
-      projectId: o == null || o.trim().length == 0 ?
-        env.BF_PID : o.trim()
-    });
+const { env } = require("process");
+const express = require("express");
+const app = express();
+app.listen((env.HEROKU && env.PORT) || 55555, () => {
+  // const { exec } = require("child_process");
+  // exec('cat ~/cardano/cfg/pid.bf', (x,o,e) => {
+  API = new BlockFrostAPI({
+    // o == null || o.trim().length == 0 ?
+    projectId: env.BF_PID, // : o.trim()
   });
+  // });
 });
 
-const path = require('path');
-const frontendBuild = '../frontend/build';
+const path = require("path");
+const { assetcdn } = require("./svc/_");
+const frontendBuild = "../frontend/build";
 app.use(express.static(path.resolve(__dirname, frontendBuild)));
 
-app.get('/latestBlock', function(req, rsp) {
-  run(API.blocksLatest(), rsp);
+app.get("/latestBlock", function (_req, _rsp) {
+  run(API.blocksLatest(), _rsp);
 });
 
-app.get('/networkInfo', function(req, rsp) {
-  run(API.network(), rsp);
+app.get("/networkInfo", function (_req, _rsp) {
+  run(API.network(), _rsp);
 });
 
-app.get('/latestEpoch', function(req, rsp) {
-  run(API.epochsLatest(), rsp);
+app.get("/latestEpoch", function (_req, _rsp) {
+  run(API.epochsLatest(), _rsp);
 });
 
-app.get('/protocolParameters', function(req, rsp) {
-  async function getLatestEpochProtocolParametersFor(rsp) {
+app.get("/protocolParameters", function (_req, _rsp) {
+  async function getLatestEpochProtocolParametersFor(_rsp) {
     const { epoch } = await API.epochsLatest();
-    run(API.epochsParameters(epoch), rsp);
+    run(API.epochsParameters(epoch), _rsp);
   }
-  getLatestEpochProtocolParametersFor(rsp);
+  getLatestEpochProtocolParametersFor(_rsp);
 });
 
-app.get('/health', function(req, rsp) {
-  run(API.health(), rsp);
+app.get("/health", function (_req, _rsp) {
+  run(API.health(), _rsp);
 });
 
-app.get('/pools', function(req, rsp) {
-  run(API.pools({ order: 'desc' }), rsp);
+app.get("/pools", function (_req, _rsp) {
+  run(API.pools({ order: "desc" }), _rsp);
 });
 
-app.get('/specificAsset', function(req, rsp) {
-  const policyID  = req.query.policy_id;
-  const assetName = req.query.asset_name;
-  run(API.assetsById(policyID + assetName), rsp);
+app.get("/specificAsset", function (_req, _rsp) {
+  const policyID = _req.query.policy_id;
+  const assetName = _req.query.asset_name;
+  run(API.assetsById(policyID + assetName), _rsp);
 });
 
-app.get('*', function(req, rsp) { // Any other GET requests
-  rsp.sendFile(path.resolve(__dirname, frontendBuild, 'index.html'));
+//#region AssetCDN
+app.get("/cdn/imageUrl", function (_req, _rsp) {
+  const assetFingerprint = _req.query.fingerprint;
+  assetcdn
+    .getImgUrl(assetFingerprint)
+    .then((rsp) => _rsp.json({ url: rsp }))
+    .catch((error) => _respondError(_rsp, error));
 });
+app.get("/cdn/assetMetadata", function (_req, _rsp) {
+  const assetFingerprint = _req.query.fingerprint;
+  assetcdn
+    .getMetadataJson(assetFingerprint)
+    .then((rsp) => _rsp.json(rsp))
+    .catch((error) => _respondError(_rsp, error));
+});
+function _respondError(resp, error) {
+  resp.status(error.status_code ?? 500);
+  resp.json({
+    error: error.message ?? error,
+  });
+}
+//#endregion
 
+app.get("*", function (_req, _rsp) {
+  // Any other GET requests
+  _rsp.sendFile(path.resolve(__dirname, frontendBuild, "index.html"));
+});
